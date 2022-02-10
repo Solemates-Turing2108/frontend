6ec0e6030aacb29c98b988628ed2309b491cffd4
Markus Rossio <argitavis@gmail.com>
2022-02-09 18:25:52 -0600

    Add function to create api service in app on component mount, Add variable to referance current value of apiserviceref

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainDisplay from './Components/MainDisplay';
+import APIService from './Services/apiService';
 
 export default function App() {
+  const apiServiceRef = useRef();
+  let apiService = null;
   const [displayedShoes, setDisplayedShoes] = useState([]);
+
+  useEffect(() => {
+    createNewApiService();
+  }, []);
+
+  const createNewApiService = () => {
+    apiServiceRef.current = new APIService();
+    apiService = apiServiceRef.current;
+  };
 
   const updateDisplayedShoes = (newShoeData) => {
     setDisplayedShoes(newShoeData);
