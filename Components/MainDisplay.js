23de21c194c33a759f1fd974105d964108d742f6
Markus Rossio <argitavis@gmail.com>
2022-02-10 03:36:02 -0600

    Add function update shoe data to main display to handle setting shoe data into state from api call

--- Components/MainDisplay.js
+++ Components/MainDisplay.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, FlatList } from 'react-native';
 import ShoeCard from "./ShoeCard";
+import { getAllShoes } from '../Services/apiService';
 
 export default function MainDisplay() {
   const [shoeData, setShoeData] = useState([]);
+
+  useEffect(() => {
+    updateShoeData();
+  }, []);
+
+  const updateShoeData = async () => {
+    const { shoes } = await getAllShoes();
+    setShoeData(shoes);
+  };
 
   const renderShoeCard = ({ item }) => {
     return <ShoeCard shoeData={item}/>
